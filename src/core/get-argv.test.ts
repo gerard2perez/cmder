a9b3b26d43b617474b22b1ec5026f988ffa711cb
gerard2perez <gerard2perez@outlook.com>
2025-05-20 12:47:48 -0600

    test: update test for get-argv

--- src/core/get-argv.test.ts
+++ src/core/get-argv.test.ts
@@ -1,8 +1,5 @@
-import { mockModule } from '@g2p/cmder/test/mock-module'
 import { getArgv } from '@g2p/cmder/get-argv'
 import { describe, test, expect } from 'bun:test'
-
-await mockModule('@g2p/cmder/get-argv')
 
 describe('get-argv', () => {
   test('load only once the args', () => {
--- src/core/get-argv.ts
+++ src/core/get-argv.ts
@@ -5,10 +5,3 @@ export function getArgv() {
   }
   return argv
 }
-// async function ifTest() {
-//   const { default: mock } = await import('./get-argv.mock.test')
-//   return mock(get-argv)
-// }
-
-// const original = process.env.NODE_ENV === 'test' ? await ifTest() : get-argv
-// export { original as get-argv }
